b7951852fc0dcac4e72df270cd81279e1680aa67
Ileolami <99335756+Ileolami@users.noreply.github.com>
2026-04-22 15:13:47 +0100

    second commit

--- smart-contract-hardhat/hardhat.config.ts
+++ smart-contract-hardhat/hardhat.config.ts
@@ -2,16 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
-const RPC_URL = vars.has("RPC_URL") ? vars.get("RPC_URL") : "";
-const PRIVATE_KEY = vars.has("PRIVATE_KEY")
-  ? vars.get("PRIVATE_KEY")
-  : "0x" + "0".repeat(64);
+const ACCESS_TOKEN = vars.has("ACCESS_TOKEN") ? vars.get("ACCESS_TOKEN") : "";
+const PRIVATE_KEY = vars.has("PRIVATE_KEY") ? vars.get("PRIVATE_KEY") : "0x" + "0".repeat(64);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: RPC_URL,
+      url: ACCESS_TOKEN,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
     },
